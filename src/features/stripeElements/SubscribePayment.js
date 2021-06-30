7726d24a17a08371a40ab492c65fd113ddf6ddd8
Dimitris Stefanakis <jimstef@outlook.com>
2021-06-30 23:35:07 +0300

    bug fixes

--- src/features/stripeElements/SubscribePayment.js
+++ src/features/stripeElements/SubscribePayment.js
@@ -13,7 +13,7 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
 import {useDispatch, useSelector} from 'react-redux';
-import {useStripe} from '@stripe/stripe-react-native';
+import {CardField, useStripe} from '@stripe/stripe-react-native';
 //import stripe from 'tipsi-stripe';
 import Config from 'react-native-config';
 import axios from 'axios';
@@ -337,6 +337,27 @@ function SubscribePayment({route}) {
               disabled={!paymentSheetEnabled}
               onPress={choosePaymentOption}
             />
+            {/* <CardField
+              postalCodeEnabled={true}
+              placeholder={{
+                number: '4242 4242 4242 4242',
+              }}
+              cardStyle={{
+                backgroundColor: '#FFFFFF',
+                textColor: '#000000',
+              }}
+              style={{
+                width: '100%',
+                height: 50,
+                marginVertical: 30,
+              }}
+              onCardChange={(cardDetails) => {
+                console.log('cardDetails', cardDetails);
+              }}
+              onFocus={(focusedField) => {
+                console.log('focusField', focusedField);
+              }}
+            /> */}
           </>
         )}
         <View
